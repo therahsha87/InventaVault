4b939c7a83bed9291501997f58460cbadfb3bc77
Rahsha <therahsha87@gmail.com>
2025-10-08 07:43:47 -0700

    Create  FarcasterWrapper.tsx

---
+++ src/components/FarcasterWrapper.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+
+const FarcasterToastManager = dynamic(() => import('./FarcasterToastManager'), {
+  ssr: false,
+  loading: () => null
+})
+
+const FarcasterManifestSigner = dynamic(() => import('./FarcasterManifestSigner'), {
+  ssr: false,
+  loading: () => null
+})
+
+interface FarcasterWrapperProps {
+  children: React.ReactNode
+}
+
+export default function FarcasterWrapper({ children }: FarcasterWrapperProps): JSX.Element {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return <>{children}</>
+  }
+
+  return (
+    <FarcasterToastManager>
+      {({ onManifestSuccess, onManifestError }) => (
+        <>
+          <FarcasterManifestSigner 
+            onSuccess={onManifestSuccess}
+            onError={onManifestError}
+          />
+          {children}
+        </>
+      )}
+    </FarcasterToastManager>
+  )
+}
